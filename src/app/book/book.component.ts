8e024e6795e1faf3beb625cd4deadaf8299f0ad8
B.Z <brunozarth@gmail.com>
2022-04-17 19:02:11 -0300

    definido card a ser exibido em book.component.html

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from './model/book';
 
 @Component({
   selector: 'app-book',
---
+++ src/app/book/model/book.ts
@@ -0,0 +1,11 @@
+import { StringMapWithRename } from "@angular/compiler/src/compiler_facade_interface";
+import * as internal from "stream";
+
+export interface Book {
+
+  title: string;
+  pages: number;
+  author: string;
+  description: string;
+
+}
